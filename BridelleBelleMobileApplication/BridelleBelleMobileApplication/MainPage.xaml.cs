e71f2740b73cde0fa34a30c1537592aa5a2c3a8b
Danielle Potts <Danielle Potts>
2017-04-24 17:06:44 +0100

    Begin building UI Add colours to mark where graphics should go Begin building test click/tap gesture on images, in prepation for opening PDF's from images

--- BridelleBelleMobileApplication/BridelleBelleMobileApplication/MainPage.xaml.cs
+++ BridelleBelleMobileApplication/BridelleBelleMobileApplication/MainPage.xaml.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace BridelleBelleMobileApplication
 {
 	public partial class MainPage : ContentPage
 	{
+		Image magazine = new Image();
+		
 		public MainPage()
 		{
-			InitializeComponent();
+			magazine.Source = ImageSource.FromFile("Images\\test_cover.jpg");
+			this.LoadFromXaml(typeof(MainPage));
+			var tapGesture = new TapGestureRecognizer();
+			tapGesture.SetBinding(TapGestureRecognizer.CommandProperty,"Tap Command");
 		}
 	}
 }
