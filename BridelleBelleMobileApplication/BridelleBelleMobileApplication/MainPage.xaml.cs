cd23f6448ec19d6e99e3f9b442d14152f07b8601
MCNorthwood <HotBlockMC@gmail.com>
2017-04-24 17:36:59 +0100

    WebView
    Having difficulties setting up the pdf to work in a WebView, WebView
    does work when going to just websites, just now needs to be altered for
    pdfs.

--- BridelleBelleMobileApplication/BridelleBelleMobileApplication/MainPage.xaml.cs
+++ BridelleBelleMobileApplication/BridelleBelleMobileApplication/MainPage.xaml.cs
@@ -11,7 +11,47 @@ namespace BridelleBelleMobileApplication
 	{
 		public MainPage()
 		{
-			InitializeComponent();
-		}
+            //Label header = new Label
+            //{
+            //    Text = "WebView",
+            //    FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+            //    HorizontalOptions = LayoutOptions.Center
+            //};
+
+            //WebView webView = new WebView
+            //{
+            //    Source = new UrlWebViewSource
+            //    {
+            //        Url = "http://xamarin.com/",
+            //    },
+            //    VerticalOptions = LayoutOptions.FillAndExpand
+            //};
+
+            //this.Padding = new Thickness(10, Device.OnPlatform(20, 0, 0), 10, 5);
+
+            //this.Content = new StackLayout
+            //{
+            //    Children =
+            //    {
+            //        header,
+            //        webView
+            //    }
+            //};
+
+            Padding = new Thickness(0, 20, 0, 0);
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new CustomWebView
+                    {
+                        Uri = "BookPreview2-Ch18-Rel0417.pdf",
+                        HorizontalOptions = LayoutOptions.FillAndExpand,
+                        VerticalOptions = LayoutOptions.FillAndExpand
+                    }
+                }
+            };
+
+        }
 	}
 }
