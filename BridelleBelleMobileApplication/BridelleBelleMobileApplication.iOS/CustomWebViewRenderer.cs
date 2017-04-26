c28d7f8ec7f12e5d92f7908b66574af1a17e65a8
Danielle Potts <danielle-19-93@live.com>
2017-04-26 11:51:41 +0100

    add custom web view renderer class for iOS.

---
+++ BridelleBelleMobileApplication/BridelleBelleMobileApplication.iOS/CustomWebViewRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Foundation;
+using UIKit;
+using BridelleBelleMobileApplication;
+using BridelleBelleMobileApplication.iOS;
+
+[assembly: ExportRenderer(typeof(CustomWebView), typeof(CustomWebViewRenderer))]
+namespace BridelleBelleMobileApplication.iOS
+{
+	public class CustomWebViewRenderer : ViewRenderer<CustomWebView, UIWebView>
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<CustomWebView> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control == null)
+			{
+				SetNativeControl(new UIWebView());
+			}
+			if (e.OldElement != null)
+			{
+				// Cleanup
+			}
+			if (e.NewElement != null)
+			{
+				var customWebView = Element as CustomWebView;
+				string fileName = Path.Combine(NSBundle.MainBundle.BundlePath, string.Format("Content/{0}", WebUtility.UrlEncode(customWebView.Uri)));
+				Control.LoadRequest(new NSUrlRequest(new NSUrl(fileName, false)));
+				Control.ScalesPageToFit = true;
+			}
+		}
+	}
+}
