4b8023b59b075f39daed2b245b993f00b5d52ac4
Danielle Potts <danielle-19-93@live.com>
2017-04-28 16:37:06 +0100

    refector request building

--- BelleBridal.Client/MagazineController.cs
+++ BelleBridal.Client/MagazineController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using BelleBridal.Data;
 using Newtonsoft.Json;
@@ -7,10 +8,13 @@ namespace BelleBridal.Client
 {
 	public class MagazineController
 	{
+		private string URL = "http://localhost:53351/api/Magazine";
+
 		public async Task<Magazine> GetMagazine(long id)
 		{
 			var client = new HttpClient();
-			var result = await client.GetStringAsync("http://localhost:53351/api/Magazine/GetMagazine/" + id);
+			URL += String.Format("/GetMagazine/{0}", id);
+			var result = await client.GetStringAsync(URL);
 			return JsonConvert.DeserializeObject<Magazine>(result);
 		}
 	}
