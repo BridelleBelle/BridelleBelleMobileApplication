5d6bd24ae7049a83a2d1a4302ef2107cc03c78a7
Danielle Potts <danielle-19-93@live.com>
2017-06-02 21:34:36 +0100

    Let user choose to see more information regarding advertiser Use popup modal to allow users to pick advertiser they wanna see when there's more than one per page Display advertiser details on new page

---
+++ BridelleBelleMobileApplication/BridelleBelleMobileApplication/Views/AdvertiserChoicePopup.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using Rg.Plugins.Popup.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using BridelleBelleMobileApplication.Models;
+
+namespace BridelleBelleMobileApplication.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AdvertiserChoicePopup : PopupPage
+	{
+		List<MagazineAdvertiser> Advertisers;
+		public AdvertiserChoicePopup (IEnumerable<MagazineAdvertiser>advertisers)
+		{
+			InitializeComponent ();
+			this.Advertisers = advertisers.ToList();
+			SetUpAdChoice(this.Advertisers);
+			//Content = options;
+		}
+
+		private void SetUpAdChoice(IEnumerable<MagazineAdvertiser> advertisers)
+		{
+			var ads = advertisers.ToList();
+			var adNames = new List<string>();
+
+			foreach (var ad in ads)
+			{
+				adNames.Add(ad.Advertiser.Name);
+			}
+
+			options.ItemsSource = adNames.ToArray();
+		}
+
+		private async void OpenAdvertiser(object sender, SelectedItemChangedEventArgs e)
+		{
+			var advertiser = new MagazineAdvertiser();
+
+			foreach (var ad in this.Advertisers)
+			{
+				if (ad.Advertiser.Name == e.SelectedItem.ToString())
+				{
+					advertiser = ad;
+				}
+			}
+
+			await Navigation.PopAllPopupAsync();
+			await Navigation.PushAsync(new AdvertiserInformation(advertiser));
+		}
+	}
+}
