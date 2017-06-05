ae810c3c1f70ab742dd5330e1fbf4008299f548b
Danielle Potts <danielle-19-93@live.com>
2017-06-05 17:01:20 +0100

    add signing in add tabbed page so we can see users content begin adding registering

---
+++ BridelleBelleMobileApplication/BridelleBelleMobileApplication/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using BridelleBelleMobileApplication.Models;
+namespace BridelleBelleMobileApplication.Models
+{
+	public class User
+	{
+		[JsonProperty("id")]
+		public string Id;
+		[JsonProperty("username")]
+		public string Username;
+		[JsonProperty("magazines")]
+		public IEnumerable<string> Magazines; //stores the ids
+
+		public IEnumerable<Magazine> OwnedMagazines;
+	}
+}
---
+++ BridelleBelleMobileApplication/BridelleBelleMobileApplication/Views/UserMagazines.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using BridelleBelleMobileApplication.Helpers;
+using BridelleBelleMobileApplication.Types;
+using BridelleBelleMobileApplication.ViewModels;
+namespace BridelleBelleMobileApplication
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class UserMagazines : ContentPage
+	{
+		public UserMagazines ()
+		{
+			InitializeComponent ();
+		}
+
+		protected override async void OnAppearing()
+		{
+			if (App.SignedInUser != null)
+			{
+				try
+				{
+					var userViewModel = SetupViewModel();
+					var images = new List<Image>();
+					var imagehelper = new ImageHelper();
+					foreach (var magazine in userViewModel.OwnedMagazines)
+					{
+						magazine.Magazine.CoverImage = await imagehelper.GetImage(ImageType.CoverImages, magazine.Magazine.CoverImageFileName);
+
+					}
+
+					BindingContext = userViewModel;
+				}
+				catch(Exception exception)
+				{
+
+				}
+			}
+		}
+
+		public UserViewModel SetupViewModel()
+		{
+			var magazines = new List<MagazineViewModel>();
+
+			foreach(var m in App.SignedInUser.OwnedMagazines)
+			{
+				magazines.Add(new MagazineViewModel { Magazine = m });
+			}
+			return new UserViewModel()
+			{
+				Username = App.SignedInUser.Username,
+				OwnedMagazines = magazines
+			};
+		}
+	}
+}
