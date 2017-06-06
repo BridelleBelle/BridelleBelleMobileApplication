2e846c8c37a481f428928df8f5958f305bebfb43
Danielle Potts <danielle-19-93@live.com>
2017-06-06 12:46:29 +0100

    fix branch

---
+++ BridelleBelleMobileApplication/BridelleBelleMobileApplication/Helpers/PasswordHash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridelleBelleMobileApplication.Helpers
+{
+	public class PasswordHash
+	{
+		public string Encode(string password)
+		{
+			var bytes = new UTF8Encoding().GetBytes(password);
+			var hashBytes = System.Security.Cryptography.MD5.Create().ComputeHash(bytes);
+			return Convert.ToBase64String(hashBytes);
+		}
+	}
+}
---
+++ BridelleBelleMobileApplication/BridelleBelleMobileApplication/ViewModels/MagazineViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BridelleBelleMobileApplication.Models;
+namespace BridelleBelleMobileApplication.ViewModels
+{
+	public class MagazineViewModel
+	{
+		public IEnumerable<Magazine> Magazines;
+	}
+}
